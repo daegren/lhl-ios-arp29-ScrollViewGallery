23ac88c5b097bdc4ec84afa7bbfd391563cda648
David Mills <dave@1morepx.com>
2019-05-13 14:13:17 -0400

    Initial Commit

---
+++ ScrollView Image Gallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView Image Gallery
+//
+//  Created by David Mills on 2019-05-13.
+//  Copyright © 2019 David Mills. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
