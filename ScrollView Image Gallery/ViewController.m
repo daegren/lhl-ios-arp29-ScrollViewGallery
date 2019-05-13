37737c0844c7e9f59bbd715a1a8652b15f7e7997
David Mills <dave@1morepx.com>
2019-05-13 14:56:59 -0400

    Displaying a series of images

--- ScrollView Image Gallery/ViewController.m
+++ ScrollView Image Gallery/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (strong, nonatomic) NSArray<UIImage *> *images;
 
 @end
 
@@ -17,6 +21,59 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+
+  UIImageView *prevImageView;
+
+  for (UIImage *image in self.images) {
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    imageView.image = image;
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+
+    [self.scrollView addSubview:imageView];
+
+    // constraints
+    // width
+    [imageView.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
+    // center y
+    [imageView.centerYAnchor constraintEqualToAnchor:self.scrollView.centerYAnchor].active = YES;
+    // top
+    [imageView.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+    // bottom
+    [imageView.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+    // no translates auto-resizing mask to constraints
+    imageView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    // if this is the last image to display
+    if ([self.images indexOfObject:image] == self.images.count - 1) {
+      // attach the right anchor, to the right anchor of the scroll view
+      // need to do this for it to calculate content size
+      [imageView.rightAnchor constraintEqualToAnchor:self.scrollView.rightAnchor].active = YES;
+    }
+    // If there was a previous imageview
+    if (prevImageView) {
+      // setup a constraint from our left anchor to its right anchor
+      [imageView.leftAnchor constraintEqualToAnchor:prevImageView.rightAnchor].active = YES;
+    } else {
+      // otherwise, connect the left anchor to the left anchor of the scroll view.
+      [imageView.leftAnchor constraintEqualToAnchor:self.scrollView.leftAnchor].active = YES;
+    }
+
+    prevImageView = imageView;
+  }
+
+}
+
+#pragma mark - Lazy Properties
+
+- (NSArray<UIImage *> *)images {
+  if (!_images) {
+    _images = @[[UIImage imageNamed:@"Lighthouse-in-Field"],
+                [UIImage imageNamed:@"Lighthouse-night"],
+                [UIImage imageNamed:@"Lighthouse-zoomed"]
+                ];
+  }
+
+  return _images;
 }
 
 
