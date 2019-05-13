c07c7e7a8db6f2144ba3b1f1995cae01a90ae782
David Mills <dave@1morepx.com>
2019-05-13 15:10:07 -0400

    Image Detail View Controller

---
+++ ScrollView Image Gallery/ImageDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageDetailViewController.h
+//  ScrollView Image Gallery
+//
+//  Created by David Mills on 2019-05-13.
+//  Copyright © 2019 David Mills. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ScrollView Image Gallery/ImageDetailViewController.m
@@ -0,0 +1,42 @@
+//
+//  ImageDetailViewController.m
+//  ScrollView Image Gallery
+//
+//  Created by David Mills on 2019-05-13.
+//  Copyright © 2019 David Mills. All rights reserved.
+//
+
+#import "ImageDetailViewController.h"
+
+@interface ImageDetailViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ImageDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+  return self.imageView;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
